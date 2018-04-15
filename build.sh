a8e6cb9105fe0d6c854c3841099ef9e9a83fedcb
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:16:36 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ sed -e '/_XData32/s:register long:register _Xconst long:' \
             --disable-video-caca \
             --disable-video-dummy \
             --disable-video-opengl && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install || exit 1
-install -v -d -m755 "${SHED_FAKEROOT}/usr/share/doc/SDL-1.2.15/html"
-install -v -m644 docs/html/*.html "${SHED_FAKEROOT}/usr/share/doc/SDL-1.2.15/html"
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+install -v -d -m755 "${SHED_FAKE_ROOT}/usr/share/doc/SDL-1.2.15/html"
+install -v -m644 docs/html/*.html "${SHED_FAKE_ROOT}/usr/share/doc/SDL-1.2.15/html"
